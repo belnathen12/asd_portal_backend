930e2c9ab72b43b9e0e12a05cfe8171f93eb77f3
Bellmiro Nathen <bellmirocech@yahoo.co.id>
2021-11-29 14:36:28 +0700

    db

--- asd_portal_backend/config/server.js
+++ asd_portal_backend/config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: "127.0.0.1",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a231b63372902a68699fbeaa954a988c'),
+      secret: env("ADMIN_JWT_SECRET", "a231b63372902a68699fbeaa954a988c"),
     },
   },
 });
+// module.exports = {
+//   host: "127.0.0.1",
+// };
